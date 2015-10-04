678216704991316f23ed9ecdfdbc6503f5d78db1
Hans Fugal <hans@fb.com>
2015-10-03 22:47:08 -0700

    blinkenlights
    Basic light command working. OSX only (because overtone's midi receiver
    list is broken on windows and linux is a pita)

--- src/launchpad/core.clj
+++ src/launchpad/core.clj
@@ -1,6 +1,19 @@
 (ns launchpad.core)
+(use '[overtone.studio.midi] '[overtone.midi :only [midi-out]])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn find-launchpad []
+  (midi-find-connected-receiver "Launchpad"))
+
+(defn mk-color
+  [r g]
+  (bit-or
+   r
+   0xC
+   (bit-shift-left g 4)))
+
+(defn light
+  [m x y c]
+  (midi-note-on
+   m
+   (+ x (* 0x10 y))
+   c))
