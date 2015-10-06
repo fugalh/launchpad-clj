ee20e93d5d3577ceaa0b97c4193ac6ad8d4ca70d
Hans Fugal <hans@fb.com>
2015-10-05 21:56:14 -0700

    jon1

---
+++ src/launchpad/jon1.clj
@@ -0,0 +1,29 @@
+(ns jon1
+  (:require [launchpad.core :as launchpad]))
+;(use 'launchpad.core)
+(def lp (launchpad/find-launchpad))
+(def red (launchpad/mk-color 3 0))
+(def green (launchpad/mk-color 0 3))
+(def amber (launchpad/mk-color 3 3))
+(launchpad/reset lp)
+
+(defn light [pad x y col] (do
+                           (launchpad/light pad x y col)
+                           (wait 250)))
+
+(defn wait [ms] (Thread/sleep ms))
+
+(defn box [x y size color]
+  (dotimes [xx size]
+    (light lp (+ x xx) y color))
+  (dotimes [yy size]
+    (light lp (+ x size -1) (+ y yy) color))
+  (dotimes [xx size]
+    (light lp (+ x xx) (+ y size -1) color))
+  (dotimes [yy size]
+    (light lp x (+ yy y) color)))
+
+(box 1 1 6 amber)
+(box 0 0 8 red)
+(box 2 2 4 green)
+(box 3 3 2 red)
