8bef6991087170616075290a104653de0c453a0f
Hans Fugal <hans@fb.com>
2015-10-07 21:22:41 -0700

    commenting and housekeeping

--- src/launchpad/core.clj
+++ src/launchpad/core.clj
@@ -1,85 +1,114 @@
 (ns launchpad.core
   (:require midi))
 
-(defn- color->velocity [[red green]]
-  (bit-or red
-          0xC
-          (bit-shift-left green 4)))
-
-(defn- coord->note [[x y]]
-  (+ x
-     (* 0x10 y)))
+;; helpers
+(declare color->velocity coord->note make-state)
 
 (defprotocol ModelBehavior
-  (render [this newstate])
-  (reset [this])
-  (grid [this [x y] [red green]])
-  (top [this x [red green]])
-  (side [this y [red green]]))
+  (reset 
+    "Reset the launchpad and update the state to match."
+    [this])
+  (grid 
+    "Update the grid at (x,y) to have the given color.
+    0-indexing from the top-right.
+    red and green range 0 to 3. Amber is achieved by combining both colors."
+    [this [x y] [red green]])
+  (top
+    "Update the top row of buttons at x (0-7) to have the given color."
+    [this x [red green]])
+  (side
+    "Update the side column of buttons at y (0-7) to have the given color."
+    [this y [red green]])
+  (update!
+    "Update launchpad to this new state, wholesale."
+    [this newstate]))
 
-(declare make-state)
-
+;; grid is a vector of vectors (8x8)
+;; top is an 8-element vector
+;; side is an 8-element vector
+;;
+;; In all of the above, the value is a color (2-element vector of red and green
+;; values, ranging from 0 to 3)
 (defrecord State [grid top side])
 
+;; combine the state, the midi device, and the actions on them
 (defrecord Model [state device]
   ModelBehavior
   (grid [this [x y] [red green]]
-    (.render this (assoc-in @(.state this)
+    (.update! this (assoc-in @(.state this)
                             [:grid x y]
                             [red green])))
 
   (top [this x [red green]]
-    (.render this (assoc-in @(.state this)
+    (.update! this (assoc-in @(.state this)
                             [:top x]
                             [red green])))
 
   (side [this y [red green]]
-    (.render this (assoc-in @(.state this)
+    (.update! this (assoc-in @(.state this)
                             [:side y]
                             [red green])))
 
-  (render [this newstate]
-    ;; TODO more efficient
-    (reset! (.state this) newstate)
-    (let [state @(.state this)
+  (update! [this newstate]
+    ;; The Launchpad Mini, and probably therefore the S, updates quickly enough
+    ;; not to warrant double-buffering here, so I haven't implemented it. But if
+    ;; flickering becomes an issue, that's an option.
+    (let [oldstate (.state this)
           dev (.device this)]
       (dotimes [x 8]
         (dotimes [y 8]
+          (when (not= (get-in @oldstate [:grid x y])
+                      (get-in newstate [:grid x y]))
+            (.send dev
+                   (midi/note-on (coord->note [x y])
+                                 (color->velocity (get-in newstate
+                                                          [:grid x y])))
+                   -1)))
+        (when (not= (get-in @oldstate [:top x])
+                    (get-in newstate [:top x]))
           (.send dev
-                 (midi/note-on (coord->note [x y])
-                               (color->velocity (get-in state [:grid x y])))
+                 (midi/control-change (+ 0x68 x)
+                                      (color->velocity (get-in newstate [:top x])))
                  -1))
-        (.send dev
-               (midi/control-change (+ 0x68 x)
-                                    (color->velocity (get-in state [:top x])))
-               -1)
-        (.send dev
-               (midi/note-on (coord->note [8 x])
-                             (color->velocity (get-in state [:side x])))
-               -1))))
+        (let [y x]
+          (when (not= (get-in @oldstate [:side y])
+                      (get-in newstate [:side y]))
+            
+            (.send dev
+                   (midi/note-on (coord->note [8 y])
+                                 (color->velocity (get-in newstate [:side y])))
+                   -1))))
+      (reset! oldstate newstate)))
 
   (reset [this]
-    (.send (.device this) (midi/control-change 0 0) -1)
-    (swap! (.state this) make-state)))
+    (.send (.device this)
+           (midi/control-change 0 0)
+           -1)
+    (reset! (.state this) (make-state))))
 
 (defn make-state 
+  "Initial state."
   ([] (State. (vec (repeat 8 (vec (repeat 8 [0 0]))))
               (vec (repeat 8 [0 0]))
               (vec (repeat 8 [0 0]))))
   ([_] (make-state)))
 
 (defn make-model []
+  "Make a model with initial state, connected to the first Launchpad device found."
   (let [model (Model. (atom (make-state))
                       (midi/get-receiver "Launchpad"))]
     (.reset model)
     model))
 
-(defn make-test-model []
-  (Model. (atom (make-state))
-          (reify javax.sound.midi.Receiver
-            (close [this])
-            (send [this msg ts]))))
+;; helpers
+(defn color->velocity
+  ;; convert the (red,green) pair to the appropriate MIDI velocity
+  [[red green]]
+  (bit-or red
+          0xC
+          (bit-shift-left green 4)))
 
-;; wip - grid/top/side in terms of updating state and calling render
-;; then make render more efficient (only render differences)
-;; and don't forget to reset and render initial state
+(defn coord->note [[x y]]
+  ;; convert the (x,y) coordinate to the appropriate MIDI note
+  (+ x
+     (* 0x10 y)))
